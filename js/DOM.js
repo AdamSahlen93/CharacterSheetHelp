1975a5c245cb97afb350753b9a8a9f4ff5422ba2
AdamSahlen93 <adam-sahlen@hotmail.com>
2022-11-07 17:17:12 +0100

    char card and stats draft

---
+++ js/DOM.js
@@ -0,0 +1,38 @@
+showHome()
+
+let character = [
+    {
+        "name": "Tanana",
+        "color": "#9F2B68",
+        "scr": "./resources/tanana.jpg",
+        "kp" : 8,
+    },
+]
+
+
+
+function showHome() {
+    document.getElementById("home").style.display = "block"
+    document.getElementById("character_sheet").style.display = "none"
+}
+
+
+function showCharacter(index) {
+   
+    document.getElementById("home").style.display = "none"
+    document.getElementById("character_sheet").style.display = "block"
+    document.getElementById("name").value = character[index].name 
+    document.getElementById("picture").src = character[index].scr
+    document.getElementById("bottom-line").style.background = character[index].color
+   
+}
+
+
+
+function addKp() {
+
+}
+
+
+
+
