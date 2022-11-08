651d9ae744ca20b7eafa5b24872f7c42917aadb6
AdamSahlen93 <adam-sahlen@hotmail.com>
2022-11-08 11:07:21 +0100

    Character template v.1

--- js/DOM.js
+++ js/DOM.js
@@ -2,12 +2,78 @@ showHome()
 
 let character = [
     {
-        "name": "Tanana",
+        "firstname": "Tanana",
+        "lastname": "den äldre",
         "color": "#9F2B68",
         "scr": "./resources/tanana.jpg",
         "kp" : 8,
+        "head": 4,
+        "chest": 5,
+        "stomach": 4,
+        "rightarm": 3,
+        "leftarm": 3,
+        "rightleg": 4,
+        "leftleg": 4,
+    },
+    {
+        "firstname": "Jens",
+        "lastname": "Nord",
+        "color": "#50C878",
+        "scr": "./resources/jens.jpg",
+        "kp" : 15,
+        "head": 5,
+        "chest": 6,
+        "stomach": 5,
+        "rightarm": 4,
+        "leftarm": 4,
+        "rightleg": 5,
+        "leftleg": 5,
+    },
+    {
+        "firstname": "Svarte",
+        "lastname": "My",
+        "color": "#8B0000",
+        "scr": "./resources/my.jpg",
+        "kp" : 8,
+        "head": 4,
+        "chest": 5,
+        "stomach": 4,
+        "rightarm": 3,
+        "leftarm": 3,
+        "rightleg": 4,
+        "leftleg": 4,
+    },
+    {
+        "firstname": "Skritz",
+        "lastname": "Cragnas",
+        "color": "#C04000",
+        "scr": "./resources/crag.jpg",
+        "kp" : 11,
+        "head": 4,
+        "chest": 5,
+        "stomach": 4,
+        "rightarm": 3,
+        "leftarm": 3,
+        "rightleg": 4,
+        "leftleg": 4,
+    },
+    {
+        "firstname": "Torbjörn",
+        "lastname": "Haraldsson",
+        "color": "#6F4E37",
+        "scr": "./resources/torbjörn.jpg",
+        "kp" : 10,
+        "head": 4,
+        "chest": 5,
+        "stomach": 4,
+        "rightarm": 3,
+        "leftarm": 3,
+        "rightleg": 4,
+        "leftleg": 4,
     },
 ]
+
+
 
 
 
@@ -21,13 +87,18 @@ function showCharacter(index) {
    
     document.getElementById("home").style.display = "none"
     document.getElementById("character_sheet").style.display = "block"
-
-    document.getElementById("name").value = character[index].name 
+    document.getElementById("firstname").value = character[index].firstname 
+    document.getElementById("lastname").value = character[index].lastname 
     document.getElementById("picture").src = character[index].scr
     document.getElementById("bottom-line").style.background = character[index].color
     document.getElementById("kp").value = character[index].kp
-
-   
+    document.getElementById("head").value = character[index].head
+    document.getElementById("chest").value = character[index].chest
+    document.getElementById("stomach").value = character[index].stomach
+    document.getElementById("right-arm").value = character[index].rightarm
+    document.getElementById("left-arm").value = character[index].leftarm
+    document.getElementById("right-leg").value = character[index].rightleg
+    document.getElementById("left-leg").value = character[index].leftleg
 }
 
 
